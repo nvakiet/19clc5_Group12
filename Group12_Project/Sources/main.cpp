454079abbf43755a0768239aa9c73cafe270aea2
Kagvn <khang30012001@gmail.com>
2020-05-12 09:55:10 +0700

    2.5, 2.6, 2.7

--- Group12_Project/Sources/main.cpp
+++ Group12_Project/Sources/main.cpp
@@ -1,214 +1,214 @@
 #include "../Headers/main.h"
 int main() {
-	Account user;
-	Semester currentSemester;
-	int choice = 0; //Represent user's choice throughout the program
-	while (choice != 2) {
-		//Show startup menu
-		choice = menuStart();
-		//Log into user account
-		if (choice == 1) {
-			if (logIn(user)) {
-				cout << "Login successfully" << endl << "\nWelcome, user " << user.username << '!' << endl;
-				pause();
-			}
-			else {
-				cerr << "Login failed! Please try again." << endl;
-				pause();
-				continue;
-			}
-		}
-		currentSemester = getCurrentSemester();
-		if (updateSemester(currentSemester))
-			cout << "The current semester has been updated!" << endl;
-		else cerr << "Failed to update the current semester. Some functions may not work properly!" << endl;
-		//Show main menu based on account's role
-		//STAFF MENU
-		while (user.role == 1) {	
-			choice = menuStaff();
-			//View staff profile
-			if (choice == 1) {
-				viewProfile(user);
-				pause();
-			}
-			//Change staff password
-			if (choice == 2) {
-				if (changePassword(user)) {
-					cout << "Your password has been changed successfully!" << endl;
-					pause();
-					continue;
-				}
-				else {
-					cerr << "Failed to change password! Please try again." << endl;
-					pause();
-					continue;
-				}
-			}
-			//CLASSES MENU
-			if (choice == 3) {
-				choice = 0;
-				while (choice != 8) {
-					choice = menuClass();
-					if (choice == 1) {
-						if (importClass())
-							cout << "Import class successfully!" << endl;
-						else cerr << "Failed to import class!" << endl;
-						pause();
-					}
-					/*Do something with classes data here
+	//Account user;
+	//Semester currentSemester;
+	//int choice = 0; //Represent user's choice throughout the program
+	//while (choice != 2) {
+	//	//Show startup menu
+	//	choice = menuStart();
+	//	//Log into user account
+	//	if (choice == 1) {
+	//		if (logIn(user)) {
+	//			cout << "Login successfully" << endl << "\nWelcome, user " << user.username << '!' << endl;
+	//			pause();
+	//		}
+	//		else {
+	//			cerr << "Login failed! Please try again." << endl;
+	//			pause();
+	//			continue;
+	//		}
+	//	}
+	//	currentSemester = getCurrentSemester();
+	//	if (updateSemester(currentSemester))
+	//		cout << "The current semester has been updated!" << endl;
+	//	else cerr << "Failed to update the current semester. Some functions may not work properly!" << endl;
+	//	//Show main menu based on account's role
+	//	//STAFF MENU
+	//	while (user.role == 1) {	
+	//		choice = menuStaff();
+	//		//View staff profile
+	//		if (choice == 1) {
+	//			viewProfile(user);
+	//			pause();
+	//		}
+	//		//Change staff password
+	//		if (choice == 2) {
+	//			if (changePassword(user)) {
+	//				cout << "Your password has been changed successfully!" << endl;
+	//				pause();
+	//				continue;
+	//			}
+	//			else {
+	//				cerr << "Failed to change password! Please try again." << endl;
+	//				pause();
+	//				continue;
+	//			}
+	//		}
+	//		//CLASSES MENU
+	//		if (choice == 3) {
+	//			choice = 0;
+	//			while (choice != 8) {
+	//				choice = menuClass();
+	//				if (choice == 1) {
+	//					if (importClass())
+	//						cout << "Import class successfully!" << endl;
+	//					else cerr << "Failed to import class!" << endl;
+	//					pause();
+	//				}
+	//				/*Do something with classes data here
 
 
 
-					*/
-				}
-				continue;
-			}
-			//COURSES MENU
-			if (choice == 4) {
-				choice = 0;
-				while (choice != 14) {
-					choice = menuCourse();
-					if (choice == 1) {
-						viewSemesterList();
-						pause();
-					}
-					if (choice == 2) {
-						cout << "Current semester: " << endl;
-						cout << "Academic year: " << currentSemester.year << endl;
-						cout << "Semester: " << currentSemester.semester << endl;
-						pause();
-					}
-					if (choice == 3) {
-						if (deleteSemester()) {
-							cout << "The semester has been deleted!" << endl;
-							pause();
-						}
-						else {
-							cerr << "Failed to delete the semester!" << endl;
-							pause();
-						}
-					}
-					if (choice == 4) {
-						if (importCourse()) {
-							cout << "The course list has been imported successfully!" << endl;
-							pause();
-						}
-						else {
-							cerr << "Failed to import the course list! Please check the data in your file." << endl;
-							pause();
-						}
-					}
-					if (choice == 5) {
-						if (addCourse()) {
-							cout << "The course has been added successfully!" << endl;
-							pause();
-						}
-						else {
-							cerr << "Failed to add the course! Please check the input data." << endl;
-							pause();
-						}
-					}
-					/*	 DO SOMETHING WITH COURSE DATA HERE
-					
-					
-					*/
-				}
-				continue;
-			}
-			//SEARCH - VIEW - EXPORT SCOREBOARD
-			if (choice == 5) {
-				SearchandViewScoreboard();
-				pause();
-			}
-			//SEARCH - VIEW - EXPORT ATTENDANCE LIST
-			if (choice == 6) {
-				SearchandViewAttendance();
-				pause();
-			}
-			//LOG OUT
-			if (choice == 7) {
-				logOut(user);
-				cout << "The account has been logged out!" << endl;
-				pause();
-			}
-		}
+	//				*/
+	//			}
+	//			continue;
+	//		}
+	//		//COURSES MENU
+	//		if (choice == 4) {
+	//			choice = 0;
+	//			while (choice != 14) {
+	//				choice = menuCourse();
+	//				if (choice == 1) {
+	//					viewSemesterList();
+	//					pause();
+	//				}
+	//				if (choice == 2) {
+	//					cout << "Current semester: " << endl;
+	//					cout << "Academic year: " << currentSemester.year << endl;
+	//					cout << "Semester: " << currentSemester.semester << endl;
+	//					pause();
+	//				}
+	//				if (choice == 3) {
+	//					if (deleteSemester()) {
+	//						cout << "The semester has been deleted!" << endl;
+	//						pause();
+	//					}
+	//					else {
+	//						cerr << "Failed to delete the semester!" << endl;
+	//						pause();
+	//					}
+	//				}
+	//				if (choice == 4) {
+	//					if (importCourse()) {
+	//						cout << "The course list has been imported successfully!" << endl;
+	//						pause();
+	//					}
+	//					else {
+	//						cerr << "Failed to import the course list! Please check the data in your file." << endl;
+	//						pause();
+	//					}
+	//				}
+	//				if (choice == 5) {
+	//					if (addCourse()) {
+	//						cout << "The course has been added successfully!" << endl;
+	//						pause();
+	//					}
+	//					else {
+	//						cerr << "Failed to add the course! Please check the input data." << endl;
+	//						pause();
+	//					}
+	//				}
+	//				/*	 DO SOMETHING WITH COURSE DATA HERE
+	//				
+	//				
+	//				*/
+	//			}
+	//			continue;
+	//		}
+	//		//SEARCH - VIEW - EXPORT SCOREBOARD
+	//		if (choice == 5) {
+	//			SearchandViewScoreboard();
+	//			pause();
+	//		}
+	//		//SEARCH - VIEW - EXPORT ATTENDANCE LIST
+	//		if (choice == 6) {
+	//			SearchandViewAttendance();
+	//			pause();
+	//		}
+	//		//LOG OUT
+	//		if (choice == 7) {
+	//			logOut(user);
+	//			cout << "The account has been logged out!" << endl;
+	//			pause();
+	//		}
+	//	}
 
-		//LECTURER MENU
-		while (user.role == 2) {
-			choice = menuLecturer();
-			//View lecturer profile
-			if (choice == 1) {
-				viewProfile(user);
-				pause();
-			}
-			//Change lecturer password
-			if (choice == 2) {
-				if (changePassword(user)) {
-					cout << "Your password has been changed successfully!" << endl;
-					pause();
-					continue;
-				}
-				else {
-					cerr << "Failed to change password! Please try again." << endl;
-					pause();
-					continue;
-				}
-			}
-			/*	ADD FUNCTIONS FOR CHOICE 3 TO 9 HERE
-
-
+	//	//LECTURER MENU
+	//	while (user.role == 2) {
+	//		choice = menuLecturer();
+	//		//View lecturer profile
+	//		if (choice == 1) {
+	//			viewProfile(user);
+	//			pause();
+	//		}
+	//		//Change lecturer password
+	//		if (choice == 2) {
+	//			if (changePassword(user)) {
+	//				cout << "Your password has been changed successfully!" << endl;
+	//				pause();
+	//				continue;
+	//			}
+	//			else {
+	//				cerr << "Failed to change password! Please try again." << endl;
+	//				pause();
+	//				continue;
+	//			}
+	//		}
+	//		/*	ADD FUNCTIONS FOR CHOICE 3 TO 9 HERE
 
 
 
 
-			*/
-			//LOG OUT
-			if (choice == 10) {
-				logOut(user);
-				cout << "The account has been logged out!" << endl;
-				pause();
-			}
-		}
 
-		//STUDENT MENU
-		while (user.role == 3) {
-			choice = menuStudent();
-			//View student profile
-			if (choice == 1) {
-				viewProfile(user);
-				pause();
-			}
-			//Change student password
-			if (choice == 2) {
-				if (changePassword(user)) {
-					cout << "Your password has been changed successfully!" << endl;
-					pause();
-					continue;
-				}
-				else {
-					cerr << "Failed to change password! Please try again." << endl;
-					pause();
-					continue;
-				}
-			}
-			if (choice == 4) {
-				if (!viewcheckin(user))
-					cerr << "Failed to get information!" << endl;
-				pause();
-			}
-			if (choice == 6) {
-				if (!viewscore(user))
-					cerr << "Failed to get information!" << endl;
-				pause();
-			}
-			if (choice == 7) {
-				logOut(user);
-				cout << "The account has been logged out!" << endl;
-				pause();
-			}
-		}
 
-	}
+	//		*/
+	//		//LOG OUT
+	//		if (choice == 10) {
+	//			logOut(user);
+	//			cout << "The account has been logged out!" << endl;
+	//			pause();
+	//		}
+	//	}
 
+	//	//STUDENT MENU
+	//	while (user.role == 3) {
+	//		choice = menuStudent();
+	//		//View student profile
+	//		if (choice == 1) {
+	//			viewProfile(user);
+	//			pause();
+	//		}
+	//		//Change student password
+	//		if (choice == 2) {
+	//			if (changePassword(user)) {
+	//				cout << "Your password has been changed successfully!" << endl;
+	//				pause();
+	//				continue;
+	//			}
+	//			else {
+	//				cerr << "Failed to change password! Please try again." << endl;
+	//				pause();
+	//				continue;
+	//			}
+	//		}
+	//		if (choice == 4) {
+	//			if (!viewcheckin(user))
+	//				cerr << "Failed to get information!" << endl;
+	//			pause();
+	//		}
+	//		if (choice == 6) {
+	//			if (!viewscore(user))
+	//				cerr << "Failed to get information!" << endl;
+	//			pause();
+	//		}
+	//		if (choice == 7) {
+	//			logOut(user);
+	//			cout << "The account has been logged out!" << endl;
+	//			pause();
+	//		}
+	//	}
+
+	//}
+removeStudent();
 	return 0;
 }
--- Group12_Project/Sources/staff-class.cpp
+++ Group12_Project/Sources/staff-class.cpp
@@ -249,11 +249,58 @@ bool addStudent() {
 	ifstream fin;
 	ofstream fout;
 	string ClassID, line, DoB;
+	int choice;
 
 	Student* newStu = new Student;
 
+	string path = "./TextFiles/Classes.txt";
+	fin.open(path);
+	if (!fin.is_open()) {
+		cerr << "Cannot open file " << path;
+		return false;
+	}
+	int n;
+	getline(fin, line);
+	n = stoi(line);
+	if (n == 0) {
+		cerr << "There are no class";
+		return false;
+	}
+	Class* classes = new Class[n];
+
+	for (int i = 0; i < n; i++) {
+		getline(fin, classes[i].className);
+	}
+	fin.close();
+
+	cout << "Current classes: " << endl;
+	for (int i = 0; i < n; i++) {
+		cout << classes[i].className << endl;
+	}
 	cout << "Enter ID of the class: ";
 	getline(cin, ClassID);
+	bool flag = false;
+	for (int i = 0; i < n; i++) {
+		if (ClassID == classes[i].className)
+			flag = true;
+	}
+	if (!flag) {
+		cout << "Class does not exist, create new? Choose 1 or 0:" << endl;
+		cout << "1.Yes" << endl;
+		cout << "0.No" << endl;
+		cin >> choice;
+		while (!cin || choice < 0 || choice > 1) {
+			flushin(cin);
+			cerr << "Invalid input!" << endl;
+			cout << "--> ENTER A NUMBER FOR YOUR CHOICE: ";
+			cin >> choice;
+		}
+		flushin(cin);
+		if (choice == 0) {
+			cout << "Cancelled";
+			return false;
+		}
+	}
 	cout << "Enter student ID: ";
 	getline(cin, newStu->ID);
 	cout << "Enter student fullname: ";
@@ -291,13 +338,17 @@ bool addStudent() {
 	else //neu co file
 	{
 		fin.open(path1);
-		if (!fout.is_open()) {
+		if (!fin.is_open()) {
 			cerr << "Cannot open file " << path1;
 			return false;
 		}
 		int n1;
 		getline(fin, line);
 		n1 = stoi(line);
+		if (n1 == 0) {
+			cerr << "There are no students";
+			return false;
+		}
 		Account* oldStu;
 		oldStu = new Account[n1];
 
@@ -355,109 +406,94 @@ bool addStudent() {
 		oldStu = nullptr;
 	}
 	//add vo file classes
-	string path2 = "./TextFiles/Classes.txt";
-	bool flag2 = false;
-	if (emptyFile(path2)) {
+	//chua co class 
+	if (flag == false) {
+		fout.open(path);
+		if (!fout.is_open()) {
+			cerr << "Cannot open file " << path;
+			return false;
+		}
+		fout << n + 1 << endl;
+		for (int i = 0; i < n; i++)
+			fout << classes[i].className << endl;
+		fout << ClassID << endl;
+		fout.close();
+
+		string path2 = systemPath + ClassID + "_Students.txt";
 		fout.open(path2);
 		if (!fout.is_open()) {
 			cerr << "Cannot open file " << path2;
 			return false;
 		}
-		fout << 1 << endl
-			<< ClassID << endl;
+		fout << 1 << endl;
+		fout << newStu->ID << endl
+			<< newStu->fullname << endl
+			<< newStu->gender << endl;
+		printDate(fout, newStu->birthDate);
+		fout << 1 << endl;
+		fout << endl;
 		fout.close();
 	}
-	else {
-		int n2;
-		fin.open(path2);
+	//co class
+	if (flag == true) {
+		//string path3 = systemPath + ClassID + "_Students.txt";
+		string path3 = "./TextFiles/Test-19CLC5.txt";
+		fin.open(path3);
 		if (!fin.is_open()) {
-			cerr << "Cannot open file " << path2;
+			cerr << "Cannot open file " << path3;
 			return false;
 		}
+		int n3;
 		getline(fin, line);
-		n2 = stoi(line);
+		n3 = stoi(line);
+		if (n3 == 0) {
+			cerr << "Class is empty";
+			return false;
+		}
+		Student* oldStu = new Student[n3];
 
-		Class* classes;
-		classes = new Class[n2];
-
-		for (int i = 0; i < n2; i++)
-			getline(fin, classes[i].className);
+		for (int i = 0; i < n3; i++) {
+			getline(fin, oldStu[i].ID);
+			getline(fin, oldStu[i].fullname);
+			getline(fin, line);
+			oldStu[i].gender = line.front();
+			getline(fin, line);
+			oldStu[i].birthDate = sToDate(line);
+			getline(fin, line);
+			oldStu[i].active = stoi(line);
+			fin.ignore(INT_MAX, '\n');
+		}
 		fin.close();
 
-		for (int i = 0; i < n2; i++) {
-			if (ClassID == classes[i].className)
-				flag2 = true;
+		fout.open(path3);
+		if (!fout.is_open()) {
+			cerr << "Cannot open file " << path3;
+			return false;
 		}
-		//chua co class 
-		if (flag2 == false) {
-			fout.open(path2);
-			if (!fout.is_open()) {
-				cerr << "Cannot open file " << path2;
-				return false;
-			}
-			fout << n2 + 1 << endl;
-			for (int i = 0; i < n2; i++)
-				fout << classes[i].className << endl;
-			fout << ClassID << endl;
-			fout.close();
-		}
-		delete[] classes;
-		classes = nullptr;
-		//co class
-		if (flag2 == true) {
-			//string path3 = systemPath + ClassID + "_Students.txt";
-			string path3 = "./TextFiles/Test-19CLC5.txt";
-			fin.open(path3);
-			if (!fin.is_open()) {
-				cerr << "Cannot open file " << path3;
-				return false;
-			}
-			int n3;
-			getline(fin, line);
-			n3 = stoi(line);
-			Student* oldStu = new Student[n3];
-
-			for (int i = 0; i < n3; i++) {
-				getline(fin, oldStu[i].ID);
-				getline(fin, oldStu[i].fullname);
-				getline(fin, line);
-				oldStu[i].gender = line.front();
-				getline(fin, line);
-				oldStu[i].birthDate = sToDate(line);
-				getline(fin, line);
-				oldStu[i].active = stoi(line);
-				fin.ignore(INT_MAX, '\n');
-			}
-			fin.close();
-
-			fout.open(path3);
-			if (!fout.is_open()) {
-				cerr << "Cannot open file " << path3;
-				return false;
-			}
-			fout << n3 + 1 << endl;
-			for (int i = 0; i < n3; i++) {
-				fout << oldStu[i].ID << endl
-					<< oldStu[i].fullname << endl
-					<< oldStu[i].gender << endl;
-				printDate(fout, oldStu[i].birthDate);
-				fout << oldStu[i].active << endl;
-				fout << endl;
-			}
-			fout << newStu->ID << endl
-				<< newStu->fullname << endl
-				<< newStu->gender << endl;
-			printDate(fout, newStu->birthDate);
-			fout << 1 << endl;
+		fout << n3 + 1 << endl;
+		for (int i = 0; i < n3; i++) {
+			fout << oldStu[i].ID << endl
+				<< oldStu[i].fullname << endl
+				<< oldStu[i].gender << endl;
+			printDate(fout, oldStu[i].birthDate);
+			fout << oldStu[i].active << endl;
 			fout << endl;
-
-			fout.close();
-			delete[] oldStu;
-			oldStu = nullptr;
 		}
+		fout << newStu->ID << endl
+			<< newStu->fullname << endl
+			<< newStu->gender << endl;
+		printDate(fout, newStu->birthDate);
+		fout << 1 << endl;
+		fout << endl;
+
+		fout.close();
+		delete[] oldStu;
+		oldStu = nullptr;
 	}
 	delete newStu;
 	newStu = nullptr;
+	delete[] classes;
+	classes = nullptr;
 	return true;
 }
 bool removeStudent() {
@@ -466,11 +502,40 @@ bool removeStudent() {
 	ofstream fout;
 	string ClassID, ID, line, DoB;
 
-	cout << "Enter classID: ";
+	string path = "./TextFiles/Classes.txt";
+	fin.open(path);
+	if (!fin.is_open()) {
+		cerr << "Cannot open file " << path;
+		return false;
+	}
+	int n;
+	getline(fin, line);
+	n = stoi(line);
+	if (n == 0) return false;
+	Class* classes = new Class[n];
+
+	for (int i = 0; i < n; i++) {
+		getline(fin, classes[i].className);
+	}
+	fin.close();
+
+	cout << "Current classes: " << endl;
+	for (int i = 0; i < n; i++) {
+		cout << classes[i].className << endl;
+	}
+	cout << "Enter class ID: ";
 	getline(cin, ClassID);
+	bool flag = false;
+	for (int i = 0; i < n; i++) {
+		if (ClassID == classes[i].className)
+			flag = true;
+	}
+	while (!flag) {
+		cout << "Enter existing class: ";
+	}
 	cout << "Enter student ID: ";
 	getline(cin, ID);
-	// in danh sach ma lop
+
 	//string path1 = systemPath + ClassID + "_Students.txt";
 	string path1 = "./TextFiles/Test-19CLC5.txt";
 	fin.open(path1);
@@ -481,6 +546,10 @@ bool removeStudent() {
 	int n1;
 	getline(fin, line);
 	n1 = stoi(line);
+	if (n1 == 0) {
+		cerr << "Class is empty";
+		return false;
+	}
 	Student* oldStu = new Student[n1];
 
 	for (int i = 0; i < n1; i++) {
@@ -497,10 +566,20 @@ bool removeStudent() {
 	fin.close();
 
 	bool* flag1 = new bool[n1] {false};
-	for (int i = 0; i < n1 && flag1[i] == false; i++) {
+	for (int i = 0; i < n1; i++) {
 		if (ID == oldStu[i].ID)
 			flag1[i] = true;
 		else flag1[i] = false;
+	}
+	//Kt ID co ton tai k
+	bool flag3 = false;
+	for (int i = 0; i < n1; i++) {
+		if (flag1[i] == true)
+			flag3 = true;
+	}
+	if (flag3 == false) {
+		cout << "ID does not exist";
+		return false;
 	}
 	fout.open(path1);
 	if (!fout.is_open()) {
@@ -534,6 +613,10 @@ bool removeStudent() {
 	int n2;
 	getline(fin, line);
 	n2 = stoi(line);
+	if (n2 == 0) {
+		cerr << "There are no students";
+		return false;
+	}
 	Account* oldAcc = new Account[n2];
 
 	for (int i = 0; i < n2; i++) {
@@ -552,10 +635,20 @@ bool removeStudent() {
 	fin.close();
 
 	bool* flag2 = new bool[n2] {false};
-	for (int i = 0; i < n2 && flag2[i] == false; i++) {
+	for (int i = 0; i < n2; i++) {
 		if (ID == oldAcc[i].username)
 			flag2[i] = true;
 		else flag2[i] = false;
+	}
+	//KT ID co trong class k
+	bool flag4 = false;
+	for (int i = 0; i < n2; i++) {
+		if (flag2[i] == true)
+			flag4 = true;
+	}
+	if (flag4 == false) {
+		cout << "ID does not exist in this class";
+		return false;
 	}
 	fout.open(path2);
 	if (!fout.is_open()) {
@@ -576,6 +669,8 @@ bool removeStudent() {
 		}
 	}
 	fout.close();
+	delete[] classes;
+	classes = nullptr;
 	delete[] flag2;
 	flag2 = nullptr;
 	delete[] oldAcc;
@@ -588,10 +683,39 @@ bool editStudent() {
 	ofstream fout;
 	string ClassID, ID, line, DoB, newID, newName, newGender, newDoB, newActive;
 
-	cout << "Enter student ID: ";
-	getline(cin, ID);
+	string path = "./TextFiles/Classes.txt";
+	fin.open(path);
+	if (!fin.is_open()) {
+		cerr << "Cannot open file " << path;
+		return false;
+	}
+	int n;
+	getline(fin, line);
+	n = stoi(line);
+	if (n == 0) return false;
+	Class* classes = new Class[n];
+
+	for (int i = 0; i < n; i++) {
+		getline(fin, classes[i].className);
+	}
+	fin.close();
+
+	cout << "Current classes: " << endl;
+	for (int i = 0; i < n; i++) {
+		cout << classes[i].className << endl;
+	}
 	cout << "Enter class ID: ";
 	getline(cin, ClassID);
+	bool flag = false;
+	for (int i = 0; i < n; i++) {
+		if (ClassID == classes[i].className)
+			flag = true;
+	}
+	while (!flag) {
+		cout << "Enter existing class: ";
+	}
+	cout << "Enter student ID: ";
+	getline(cin, ID);
 
 	//string path1 = "./TextFiles/Students.txt";
 	string path1 = "./TextFiles/Test-student.txt";
@@ -604,6 +728,7 @@ bool editStudent() {
 	int n1;
 	getline(fin, line);
 	n1 = stoi(line);
+	if (n1 == 0) return false;
 	Account* oldStu = new Account[n1];
 
 	for (int i = 0; i < n1; i++) {
@@ -626,6 +751,15 @@ bool editStudent() {
 		if (ID == oldStu[i].username)
 			flag1[i] = true;
 		else flag1[i] = false;
+	}
+	bool flag3 = false;
+	for (int i = 0; i < n1; i++) {
+		if (flag1[i] == true)
+			flag3 = true;
+	}
+	if (flag3 == false) {
+		cout << "ID does not exist";
+		return false;
 	}
 	for (int i = 0; i < n1; i++) {
 		if (flag1[i] == true) {
@@ -689,6 +823,7 @@ bool editStudent() {
 	int n2;
 	getline(fin, line);
 	n2 = stoi(line);
+	if (n2 == 0) return false;
 	Student* students = new Student[n2];
 
 	for (int i = 0; i < n2; i++) {
@@ -704,7 +839,6 @@ bool editStudent() {
 	}
 	fin.close();
 
-
 	fout.open(path2);
 	if (!fout.is_open()) {
 		cerr << "Cannot open file " << path2;
@@ -716,6 +850,15 @@ bool editStudent() {
 		if (ID == students[i].ID)
 			flag2[i] = true;
 		else flag2[i] = false;
+	}
+	bool flag4 = false;
+	for (int i = 0; i < n2; i++) {
+		if (flag2[i] == true)
+			flag4 = true;
+	}
+	if (flag4 == false) {
+		cout << "ID does not exist in this class";
+		return false;
 	}
 	for (int i = 0; i < n2; i++) {
 		if (flag2[i]) {
@@ -737,9 +880,191 @@ bool editStudent() {
 	}
 	fout.close();
 
+	delete[] classes;
+	classes = nullptr;
 	delete[] students;
 	students = nullptr;
 	delete[] flag1;
 	flag1 = nullptr;
+	return true;
+}
+bool viewClasses() {
+	ifstream fin;
+	ofstream fout;
+	string ClassID, line;
+
+	string path = "./TextFiles/Classes.txt";
+	fin.open(path);
+	if (!fin.is_open()) {
+		cerr << "Cannot open file " << path;
+		return false;
+	}
+	int n;
+	getline(fin, line);
+	n = stoi(line);
+	if (n == 0) return false;
+	Class* classes = new Class[n];
+
+	for (int i = 0; i < n; i++) {
+		getline(fin, classes[i].className);
+	}
+	fin.close();
+
+	cout << "Current classes: " << endl;
+	for (int i = 0; i < n; i++) {
+		cout << classes[i].className << endl;
+	}
+	delete[] classes;
+	classes = nullptr;
+	return true;
+}
+bool viewStudentsInClass() {
+	string systemPath = "./TextFiles/", fileEx = "_Students.txt";
+	ifstream fin;
+	ofstream fout;
+	string ClassID, line, DoB;
+
+	cout << "Enter class ID: ";
+	getline(cin, ClassID);
+	//string path3 = systemPath + ClassID + "_Students.txt";
+	string path = "./TextFiles/Test-19CLC5.txt";
+	fin.open(path);
+	if (!fin.is_open()) {
+		cerr << "Cannot open file " << path;
+		return false;
+	}
+	int n;
+	getline(fin, line);
+	n = stoi(line);
+	if (n == 0) return false;
+	Student* oldStu = new Student[n];
+
+	for (int i = 0; i < n; i++) {
+		getline(fin, oldStu[i].ID);
+		getline(fin, oldStu[i].fullname);
+		getline(fin, line);
+		oldStu[i].gender = line.front();
+		getline(fin, DoB);
+		oldStu[i].birthDate = sToDate(DoB);
+		getline(fin, line);
+		oldStu[i].active = stoi(line);
+		fin.ignore(INT_MAX, '\n');
+	}
+	fin.close();
+
+	for (int i = 0; i < n; i++) {
+		cout << oldStu[i].ID << endl
+			<< oldStu[i].fullname << endl
+			<< oldStu[i].gender << endl
+			<< setfill('0') << right << oldStu[i].birthDate.tm_year + 1900 << '-'
+			<< setw(2) << oldStu[i].birthDate.tm_mon + 1 << '-'
+			<< setw(2) << oldStu[i].birthDate.tm_mday << endl
+			<< oldStu[i].active << endl;
+		cout << endl;
+	}
+	delete[] oldStu;
+	oldStu = nullptr;
+	return true;
+}
+bool changeClass() {
+	string systemPath = "./TextFiles/", fileEx = "_Students.txt";
+	ifstream fin;
+	ofstream fout;
+	string ClassID, ID, line, DoB, newClass;
+
+	viewClasses();
+	cout << "Enter class ID: ";
+	getline(cin, ClassID);
+	cout << "Enter new class ID: ";
+	getline(cin, newClass);
+
+	//string path1 = systemPath + ClassID + "_Students.txt";
+	string path1 = "./TextFiles/Test-19CLC5.txt";
+	fin.open(path1);
+	if (!fin.is_open()) {
+		cerr << "Cannot open file " << path1;
+		return false;
+	}
+	int n1;
+	getline(fin, line);
+	n1 = stoi(line);
+	if (n1 == 0) return false;
+	Student* oldStu1 = new Student[n1];
+
+	for (int i = 0; i < n1; i++) {
+		getline(fin, oldStu1[i].ID);
+		getline(fin, oldStu1[i].fullname);
+		getline(fin, line);
+		oldStu1[i].gender = line.front();
+		getline(fin, DoB);
+		oldStu1[i].birthDate = sToDate(DoB);
+		getline(fin, line);
+		oldStu1[i].active = stoi(line);
+		fin.ignore(INT_MAX, '\n');
+	}
+	fin.close();
+
+	//string path2 = systemPath + ClassID + "_Students.txt";
+	string path2 = "./TextFiles/Test-19CLC7.txt";
+	fin.open(path2);
+	if (!fin.is_open()) {
+		cerr << "Cannot open file " << path2;
+		return false;
+	}
+	int n2;
+	getline(fin, line);
+	n2 = stoi(line);
+	if (n2 == 0) return false;
+	Student* oldStu2 = new Student[n2];
+
+	for (int i = 0; i < n2; i++) {
+		getline(fin, oldStu2[i].ID);
+		getline(fin, oldStu2[i].fullname);
+		getline(fin, line);
+		oldStu2[i].gender = line.front();
+		getline(fin, DoB);
+		oldStu2[i].birthDate = sToDate(DoB);
+		getline(fin, line);
+		oldStu2[i].active = stoi(line);
+		fin.ignore(INT_MAX, '\n');
+	}
+	fin.close();
+
+	fout.open(path2);
+	if (!fout.is_open()) {
+		cerr << "Cannot open file " << path2;
+		return false;
+	}
+	fout << n1 << endl;
+	for (int i = 0; i < n1; i++) {
+		fout << oldStu1[i].ID << endl
+			<< oldStu1[i].fullname << endl
+			<< oldStu1[i].gender << endl;
+		printDate(fout, oldStu1[i].birthDate);
+		fout << oldStu1[i].active << endl;
+		fout << endl;
+	}
+	fout.close();
+
+	fout.open(path1);
+	if (!fout.is_open()) {
+		cerr << "Cannot open file " << path1;
+		return false;
+	}
+	fout << n2 << endl;
+	for (int i = 0; i < n2; i++) {
+		fout << oldStu2[i].ID << endl
+			<< oldStu2[i].fullname << endl
+			<< oldStu2[i].gender << endl;
+		printDate(fout, oldStu2[i].birthDate);
+		fout << oldStu2[i].active << endl;
+		fout << endl;
+	}
+	fout.close();
+
+	delete[] oldStu1;
+	oldStu1 = nullptr;
+	delete[] oldStu2;
+	oldStu2 = nullptr;
 	return true;
 }
